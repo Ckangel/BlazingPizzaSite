0cb225a3274448e373b1eed8b8279109287d389b
Charles Kingsley Ajeigbe <ckajeigbe1@gmail.com>
2026-05-23 21:49:40 +0000

    Debug and update project

--- obj/Debug/net8.0/BlazingPizzaSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazingPizzaSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96026d9d43f1747395474f65c106e8a1a1b77b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0a67f3eb2e6d54daf69bdfa0a22893b95200b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
