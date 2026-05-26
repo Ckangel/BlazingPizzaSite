a5f54e591a68ffec77f65da79f62f6ca1de4f9f2
Charles Kingsley Ajeigbe <ckajeigbe1@gmail.com>
2026-05-26 10:36:44 +0000

    Fixed namespace and others successfully

--- obj/Debug/net10.0/BlazingPizzaSite.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazingPizzaSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c212180b0a5023dab950ac5322e069f1b5bd55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a5f1e5b6899ee9dd3d18d444d7eb3d0fc28c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
